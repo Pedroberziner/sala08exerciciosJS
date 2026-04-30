c4d23edd5934badf34d9b05824cdd22955e73d56
Pedro <123497576+Pedroberziner@users.noreply.github.com>
2026-04-30 07:52:16 -0300

    Add files via upload

--- calculos/script.js
+++ calculos/script.js
@@ -1,39 +1,34 @@
-
 function somar(){
-    var n1 = document.getElementById("n1").valueAsNumber;
-    var n2 = document.getElementById("n2").valueAsNumber;
-
-    var resposta = document.getElementById("resposta")
-    resposta.textContent = n1 + n2
+    var n1 = document.getElementById("n1").valueAsNumber
+    var n2 = document.getElementById("n2").valueAsNumber 
+    var resp = document.getElementById("resposta")
+    resp.textContent = n1 + n2
 }
 
-function Subtração(){
-    var n3 = document.getElementById("n3").valueAsNumber;
-    var n4 = document.getElementById("n4").valueAsNumber;
-
-    var respsub = document.getElementById("respsub")
-    respsub.textContent = n3 - n4
+function sub(){
+    var n3 = document.getElementById("n3").valueAsNumber
+    var n4 = document.getElementById("n4").valueAsNumber 
+    var resp = document.getElementById("respSub")
+    resp.textContent = n3 - n4
 }
 
-
-function multiplicar(){
-    var n5 = document.getElementById("n5").valueAsNumber;
-    var n6 = document.getElementById("n6").valueAsNumber;
-
-    var respmult = document.getElementById("respmult")
-    respmult.textContent = n5 * n6
+function multi(){
+    var n3 = document.getElementById("n5").valueAsNumber
+    var n4 = document.getElementById("n6").valueAsNumber 
+    var resp = document.getElementById("respMulti")
+    resp.textContent = n3 * n4
 }
 
-function dividir(){
-    var n7 = document.getElementById("n7").valueAsNumber;
-    var n8 = document.getElementById("n8").valueAsNumber;
-
-    var respdiv = document.getElementById("respdiv")
-    
-    
-    if(n8 == 0) {
-        respdiv.textContent = "Não é possível dividir por zero"
-    } else {
-        respdiv.textContent = n7 / n8
+function div(){
+    var n3 = parseFloat(document.getElementById("n7").value)
+    var n4 = parseFloat(document.getElementById("n8").value)
+    var resp = document.getElementById("respDiv")
+    if(n4 !== 0){
+        resp.textContent = n3 / n4
+    }else{
+        resp.textContent = "Não se divide por 0"
     }
 }
+
+
+
